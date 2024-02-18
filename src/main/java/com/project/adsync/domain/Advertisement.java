24b8bc15f740fe9e09d4d57ee1a5689be577392f
ThanujaH <thanujahennayake@gmail.com>
2024-02-18 19:18:43 +0530

    Spring version and java version bump + imports updated to suit java 17

--- src/main/java/com/project/adsync/domain/Advertisement.java
+++ src/main/java/com/project/adsync/domain/Advertisement.java
@@ -1,9 +1,10 @@
 package com.project.adsync.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+
 
 @Data
 @NoArgsConstructor
